c08dec122da5a7980972142dda3202bdbcfe99f8
zhizhiHH Jin <3504224599@qq.com>
2023-03-22 03:54:04 +0000

    AND

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# 存储留言
 messages = []
 
-# 主页路由，包括展示所有留言和留言表单
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        # 获取留言内容，并且追加到留言列表中
-        message = request.form["message"]
-        messages.append(message)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        message = request.form['message']
+        name = request.form['name']
+        messages.append((name, message))
+    return render_template('index.html.jinja2', messages=messages)
 
-    # 渲染主页模板，传入留言列表 messages
-    return render_template("index.html.jinja2", messages=messages)
-
-# 程序入口
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+if __name__ == '__main__':
+ app.run(debug=True)
